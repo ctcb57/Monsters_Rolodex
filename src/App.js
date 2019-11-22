8c5fb74ffc24eb41a98c1f1fe7d80800248f0d09
Charles Clark <ctcb57@gmail.com>
2019-11-22 00:26:48 -0600

    Created card and card-list components along with css styling for each of these components.  Functionality confirmed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.components';
 import './App.css';
 
 class App extends Component{
@@ -7,26 +7,24 @@ class App extends Component{
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein'
-        },
-        {
-          name: 'Dracula'
-        },
-        {
-          name: 'Zombie'
-        }
-      ]
-    }
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}));
   }
 
   render(){
     return (
       <div className="App">
+        <CardList monsters={this.state.monsters}>
         {this.state.monsters.map(monster => (
-            <h1> {monster.name} </h1>
-          ))}
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
+        </CardList>
       </div>
     );
   }
